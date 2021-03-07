b4a375474879a5590875f07b584d46e46308d9cb
Dick Justice <dickdangerjustice@gmail.com>
2021-03-07 11:51:49 -0500

    Add code to go along with useful tools video.

---
+++ ValheimConfigAndDebugging/README.md
@@ -0,0 +1,12 @@
+# Config and debugging sample
+
+Sample mod to go along with video at https://www.youtube.com/watch?v=EaEFMf-Zj1A.
+
+Required libraries:
+BepInEx\core\0Harmony.dll
+BepInEx\core\BepInEx.dll
+valheim_Data\Managed\assembly_valheim.dll
+valheim_Data\Managed\UnityEngine.dll
+valheim_Data\Managed\UnityEngine.CoreModule.dll
+
+Dll is included alongside this readme for testing purposes.
---
+++ ValheimConfigAndDebugging/ValheimConfigAndDebugging.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using BepInEx.Configuration;
+
+namespace ValheimConfigAndDebugging
+{
+    [BepInPlugin("dickdangerjustice.ValheimConfigAndDebugging", "Valheim Config and Debugging", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class ValheimConfigAndDebugging : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("dickdangerjustice.ValheimConfigAndDebugging");
+        private static ConfigEntry<float> sailForceFactorMultiplier;
+
+        void Awake()
+        {
+            sailForceFactorMultiplier = Config.Bind<float>("General", "SailForceFactorMultiplier", 1f, "Sail Force Factor Multiplier");
+
+            Debug.Log("Initialized config and debugging");
+            harmony.PatchAll();
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        [HarmonyPatch(typeof(Ship), "Awake")]
+        static class Ship_Awake_Patch
+        {
+            static void Postfix(ref float ___m_sailForceFactor)
+            {
+                Debug.Log($"Old sailForceFactor: {___m_sailForceFactor}");
+                ___m_sailForceFactor *= sailForceFactorMultiplier.Value;
+                Debug.Log($"New sailForceFactor: {___m_sailForceFactor}");
+            }
+        }
+    }
+}
